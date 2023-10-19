4deea996a92a1cfe33ad44889827cd278b381601
Gautamagarwal563 <gagarwa1@terpmail.umd.edu>
2023-10-18 22:38:26 -0400

    initial commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+def greet_user():
+    # Prompt the user for their name
+    user_name = input("Please enter your name: ")
+
+    # Greet the user
+    print(f"Hello, {user_name}! Welcome to the Python Greeting Script.")
+
+if __name__ == "__main__":
+    greet_user()
